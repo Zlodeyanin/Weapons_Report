01b52e437fe4c01bad411575c7ebb7f4e8163910
Zlodeyanin <zlodeyanin0@gmail.com>
2023-06-02 23:01:41 +0300

    Добавьте файлы проекта.

---
+++ Weapons_Report/Program.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Weapons_Report
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MilitaryUnit militaryUnit = new MilitaryUnit();
+            militaryUnit.Work();
+        }
+    }
+
+    class Soldier
+    {
+        public Soldier() 
+        {
+            int minQuantityMounts = 0;
+            int maxQuantityMounts = 13;
+            Name = UserUtils.GenerateRandomName();
+            Weapon = UserUtils.GenerateRandomWeapon();
+            Rank = UserUtils.GenerateRandomRank();
+            Tour = UserUtils.GenerateRandomTour(minQuantityMounts, maxQuantityMounts);
+        }
+
+        public string Name { get; private set; }
+        public string Weapon { get; private set; }
+        public string Rank { get; private set; }
+        public int Tour { get; private set; }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"{Name} .Оружие - {Weapon} .Звание - {Rank}. Срок службы - {Tour} месяцев.");
+        }
+    }
+
+    class UserUtils
+    {
+        private static Random _random = new Random();
+
+        public static string GenerateRandomName()
+        {
+            string[] names = { "Иван", "Евгений", "Василий", "Владимир", "Вячеслав" };
+            string name = "";
+            return name += names[_random.Next(names.Length)];
+        }
+
+        public static string GenerateRandomWeapon()
+        {
+            string[] weapons = { "Автомат", "Пулёмет", "Винтовка", "Штык нож", "Ракетница" };
+            string weapon = "";
+            return weapon += weapons[_random.Next(weapons.Length)];
+        }
+
+        public static string GenerateRandomRank()
+        {
+            string[] ranks = { "Старшина", "Рядовой", "Майор"};
+            string rank = "";
+            return rank += ranks[_random.Next(ranks.Length)];
+        }
+
+        public static int GenerateRandomTour(int min, int max)
+        {
+            return _random.Next(min, max);
+        }
+    }
+
+    class MilitaryUnit
+    {
+        private List<Soldier> _soldiers = new List<Soldier>();
+
+        public MilitaryUnit() 
+        {
+            CreateSoldiers();
+        }
+
+        public void Work()
+        {
+            const string CommandShowAllSoldersInfo = "1";
+            const string CommandShowNameAndRankSoldiers = "2";
+            const string CommandExit = "3";
+
+            bool isWork = true;
+
+            while (isWork)
+            {
+                Console.WriteLine($"Введите {CommandShowAllSoldersInfo}, чтобы вывести всю информацию о солтадах.");
+                Console.WriteLine($"Введите {CommandShowNameAndRankSoldiers}, чтобы вывести только имена и звания солдат.");
+                Console.WriteLine($"Введите {CommandExit}, чтобы завершить работу.");
+                string userInput = Console.ReadLine();
+
+                switch (userInput)
+                {
+                    case CommandShowAllSoldersInfo:
+                        ShowAllSoldersInfo();
+                        break;
+
+                        case CommandShowNameAndRankSoldiers:
+                        ShowNameAndRankSoldiers();
+                        break;
+
+                        case CommandExit:
+                        isWork= false;
+                        break;
+
+                    default:
+                        Console.WriteLine("Нет такой команды...");
+                        break;
+                }
+
+                Console.ReadKey();
+                Console.Clear();
+            }
+        }
+
+        private void CreateSoldiers()
+        {
+            int quantitySoldiers = 10;
+
+            for (int i = 0; i < quantitySoldiers; i++)
+            {
+                _soldiers.Add(new Soldier());
+            }
+        }
+
+        private void ShowAllSoldersInfo()
+        {
+            foreach (Soldier soldier in _soldiers)
+            {
+                soldier.ShowInfo();
+            }
+        }
+
+        private void ShowNameAndRankSoldiers()
+        {
+            var nameAndRankSoldiers = _soldiers.Select(soldier => new { Name = soldier.Name, Rank = soldier.Rank });
+
+            foreach (var soldier in nameAndRankSoldiers)
+            {
+                Console.WriteLine($"{soldier.Name} .Звание - {soldier.Rank} .");
+            }
+        }
+    }
+}
